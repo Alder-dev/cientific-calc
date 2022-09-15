728fea08469d31f1659d5a891f259f78c5ab4b4a
AlderDev <pkeiner08@gmail.com>
2022-09-14 19:56:58 -0500

    3rd commit

--- js/calc.js
+++ js/calc.js
@@ -4,6 +4,7 @@ var usePoint = "no";
 var numWait = 0;
 var opWait = "";
 var solution = "";
+let porcentCalc = document.getElementById("porcentCalc");
 
 // Function: numPress -> Presionar un numero
 function ingNum(x) {
@@ -31,15 +32,106 @@ function ingNum(x) {
 // Function: opPress -> Presionar un operador
 function ingOp(y) {
   if (opWait == "") {
-    numWait = numScreen;
+    numWait = document.calc.inputOp.value;
     document.calc.inputOp.value += y;
     opWait = y;
-    screenWinum = "yes";
+    screenWinum = "no";
     numScreen = "";
     usePoint = "no";
   }
 }
+// Function: calcResult -> Calcular el resultado
+function calcResult() {
+  if (opWait != "") {
+    solution = numWait + opWait + numScreen;
+    document.calc.inputRes.value = eval(solution);
+    numScreen = eval(solution);
+    screenWinum = "yes";
+    opWait = "";
+    usePoint = "no";
+  }
+}
 
-function calcResult(){
-    
+// Function: multi -> Multiplicar
+function multi() {
+    if (opWait == "") {
+        numWait = document.calc.inputOp.value;
+        document.calc.inputOp.value += "*";
+        opWait = "*";
+        screenWinum = "no";
+        numScreen = "";
+        usePoint = "no";
+    }
+}
+
+// Function: raiz -> Calcular la raiz cuadrada
+function raiz() {
+  if (opWait == "") {
+    document.calc.inputRes.value = Math.sqrt(numScreen);
+    screenWinum = "yes";
+    opWait = "";
+    usePoint = "no";
+  }
+}
+// Function: seno -> Calcular el seno
+function seno() {
+  if (opWait == "") {
+    document.calc.inputRes.value = Math.sin(numScreen);
+    screenWinum = "yes";
+    opWait = "";
+    usePoint = "no";
+  }
+}
+// Function: coseno -> Calcular el coseno
+function coseno() {
+    if (opWait == "") {
+        document.calc.inputRes.value = Math.cos(numScreen);
+        screenWinum = "yes";
+        opWait = "";
+        usePoint = "no";
+    }
+}
+
+// Function: tangente -> Calcular la tangente
+function tangente() {
+    if (opWait == "") {
+        document.calc.inputRes.value = Math.tan(numScreen);
+        screenWinum = "yes";
+        opWait = "";
+        usePoint = "no";
+    }
+}
+
+// Function: logaritmo -> Calcular el logaritmo
+function log() {
+    if (opWait == "") {
+        document.calc.inputRes.value = Math.log10(numScreen);
+        screenWinum = "yes";
+        opWait = "";
+        usePoint = "no";
+    }
+}
+
+// Function: porcent -> Calcular el porcentaje
+function porcent(num, porcentCalc) {
+    if (opWait == "") {
+        document.calc.inputRes.value = num * porcentCalc / 100;
+        screenWinum = "yes";
+        opWait = "";
+        usePoint = "no";
+    }
+
+    return (num * porcent) / 100;
+}
+
+// Function: Clear -> Limpiar la pantalla
+function clearInput() {
+  numScreen = "0";
+  screenWinum = "yes";
+  usePoint = "no";
+  numWait = 0;
+  opWait = "";
+  solution = "";
+  document.calc.inputOp.value = "0";
+  document.calc.inputRes.value = "";
 }
